9fdca5d479016cc4b21db3b043388d03229494b6
yatharth <yatharthramteke786@gmail.com>
2026-06-17 11:27:41 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <div className="">
+      <h1 className='text-red-200'>Koseli Project</h1>
+     </div>
+    </>
+  )
+}
+
+export default App
